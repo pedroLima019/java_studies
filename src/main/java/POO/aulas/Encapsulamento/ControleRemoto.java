83f3d5883b086e089c95dc60a9db6a9fbdd43aeb
pedroLima019 <pedroliveiralima21@gmail.com>
2026-06-26 10:46:28 -0300

    add class about encapsulament

---
+++ src/main/java/POO/aulas/Encapsulamento/ControleRemoto.java
@@ -0,0 +1,110 @@
+package POO.aulas.Encapsulamento;
+
+
+public class ControleRemoto implements Controlador {
+    private int volume;
+    private boolean ligado;
+    private boolean tocando;
+
+    //constructor
+    public ControleRemoto() {
+        volume = 50;
+        ligado = false;
+        tocando = false;
+    }
+
+    // GETTERS and SETTERS
+    private int getVolume() {
+        return volume;
+    }
+
+    private void setVolume(int volume) {
+        this.volume = volume;
+    }
+
+    private boolean getLigado() {
+        return ligado;
+    }
+
+    private void setLigado(boolean ligado) {
+        this.ligado = ligado;
+    }
+
+    private boolean getTocando() {
+        return tocando;
+    }
+
+    private void setTocando(boolean tocando) {
+        this.tocando = tocando;
+    }
+
+    //Methods of interface
+    @Override
+    public void ligar() {
+        setLigado(true);
+    }
+
+    @Override
+    public void desligar() {
+        setLigado(false);
+    }
+
+    @Override
+    public void abrirMenu() {
+        System.out.println("Ligado: " + getLigado());
+        System.out.println("Volume: " + getVolume());
+        for (int i = 0; i <= this.getVolume(); i += 10) {
+            System.out.print("/");
+        }
+    }
+
+    @Override
+    public void fecharMenu() {
+        System.out.println("Fechando Menu");
+    }
+
+    @Override
+    public void maisVolume() {
+        if (this.getLigado()) {
+            this.setVolume(this.getVolume() + 1);
+        }
+    }
+
+    @Override
+    public void menosVolume() {
+        if (this.getLigado()) {
+            this.setVolume(this.getVolume() - 1);
+        }
+    }
+
+    @Override
+    public void ligarMudo() {
+        if (this.getLigado() && this.getVolume() > 0) {
+            this.setVolume(0);
+        }
+    }
+
+    @Override
+    public void desligarMudo() {
+        if (this.getLigado() && this.getVolume() == 0) {
+            this.setVolume(50);
+            System.out.println("Desligando Mudo");
+        }
+    }
+
+    @Override
+    public void play() {
+        if (this.getLigado() && !this.getTocando()) {
+            this.setTocando(true);
+            System.out.println("Play");
+        }
+    }
+
+    @Override
+    public void pause() {
+        if (this.getLigado() && this.getTocando()) {
+            this.setTocando(false);
+            System.out.println("Pause");
+        }
+    }
+}
--- src/main/java/POO/aulas/Encapsulamento/Program.java
+++
@@ -1,9 +0,0 @@
-package POO.aulas.Encapsulamento;
-
-public class Program {
-    public static void main(String[] args) {
-        Filme f1 = new Filme("Seila", 12, 5);
-        f1.avaliar(10);
-        System.out.println(f1.toString());
-    }
-}
