dcfc8630a921703c438b78aaaaa1216727ab1f15
pedroLima019 <pedroliveiralima21@gmail.com>
2026-07-01 10:10:08 -0300

    create Exercicio06

---
+++ src/main/java/POO/exercicios/Exercicio06/Arquivo.java
@@ -0,0 +1,58 @@
+package POO.exercicios.Exercicio06;
+
+public class Arquivo {
+    private String nome;
+    private String conteudo;
+    private int tamanhoByte;
+    private int nivelAcesso;
+
+    public Arquivo(String nome, String conteudo) {
+        this.nome = nome;
+        this.conteudo = conteudo;
+        this.tamanhoByte = Arquivo.this.conteudo.length();
+        this.nivelAcesso = 0;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public int getTamanhoByte() {
+        return tamanhoByte;
+    }
+
+    public int getNivelAcesso() {
+        return nivelAcesso;
+    }
+
+    public boolean alterarConteudo(String novoConteudo, int nivelRequisitante) {
+        if (nivelRequisitante >= nivelAcesso) {
+            this.conteudo = novoConteudo;
+            tamanhoByte = novoConteudo.length();
+            return true;
+        }
+        return false;
+    }
+
+    public boolean alterarNivelAcesso(int novoNivel) {
+        if (novoNivel < 0 || novoNivel > 2) {
+            return false;
+        }
+
+        if (novoNivel < this.nivelAcesso) {
+            return false;
+        }
+
+        this.nivelAcesso = novoNivel;
+        return true;
+    }
+
+    public String visualizarConteudo(int nivelRequisitante) {
+        if (nivelRequisitante >= nivelAcesso) {
+
+            return conteudo;
+        }
+        return "Acesso_Negado";
+    }
+
+}
